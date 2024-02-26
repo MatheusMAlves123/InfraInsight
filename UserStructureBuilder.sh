b078bd84a7dfabfe74706242133bd60cb2d71faa
Matheus Mesquita Alves <72529680+MatheusMAlves123@users.noreply.github.com>
2024-02-26 15:40:45 -0300

    Update UserStructureBuilder.sh

--- UserStructureBuilder.sh
+++ UserStructureBuilder.sh
@@ -1,33 +1,40 @@
-#!/bin/bash
+#! /bin/bash
 
-echo "Criando diretórios..."
+echo "Criando diretórios........"
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos de usuários..."
+echo "Diretórios Criados"
+
+echo "Criando Grupos......"
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo "Grupos Criados"
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+echo "Criando Usuários......."
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
+useradd carlos -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 Teste@123) -G GRP_SEC
+
+
+echo "Permissões aos diretórios........"
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -38,4 +45,4 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Fim....."
+echo "Fim"
